bbf63480c6c5d63698a2d0e23affc9382e3315df
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-06-08 13:48:17 -0700

    browser http requests with fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
 console.log('Client side javascript file is loaded!');
+
+fetch("http://localhost:3000/weather?address=Boston")
+    .then(async response => {
+        const data = await response.json();
+
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data.location);
+            console/log(data.forecast)
+        }
+    })
+    .catch(error => console.log(error));
