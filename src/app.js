f930b67337b8e64219edb2570affe24a18abdf5f
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-06-07 21:35:08 -0700

    dynamic pages with templating ad customizing the views directory

--- src/app.js
+++ src/app.js
@@ -1,21 +1,39 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
-    res.send('Hello express!');
+    res.render('index', {
+        title: 'Weather',
+        name: 'Oguzhan Tuna'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Oguzhan Tuna'
+    });
 });
 
 app.get('/help', (req, res) => {
-    res.send('Help page');
-})
-
-app.get('/about', (req, res) => {
-    res.send("About page");
+    res.render('help', {
+        helpMessage: "This is a help message!"
+    });
 });
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page');
+    res.send({
+        forecase: 'It is so hot',
+        location: 'Arizona'
+    });
 });
 
 app.listen(3000, () => {
