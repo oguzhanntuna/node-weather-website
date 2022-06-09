9c88112c26c15da5f631adf1bb6e5e3cefa9901c
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-06-08 22:11:58 -0700

    deploying node.js to heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', event => {
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
 
-    fetch(`http://localhost:3000/weather?address=${location}`)
+    fetch(`/weather?address=${location}`)
         .then(async response => {
             const data = await response.json();
 
